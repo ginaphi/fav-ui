e1d29ed2c6526e93e0da34cfe87a308b9b8ce5b4
Fredy Ciang <63209376+fredyciang@users.noreply.github.com>
2024-09-23 16:52:50 +0800

    sample usage of hono rpc

--- apps/web/app/(routes)/page.tsx
+++ apps/web/app/(routes)/page.tsx
@@ -5,7 +5,16 @@ import Header from "@/components/header"
 import Image from "next/image"
 import Footer from "@/components/footer"
 
-export default function MarketingPage() {
+import { type AppType } from "@/app/api/[[...route]]/route"
+import { env } from "@/env.mjs"
+import { hc } from "hono/client"
+
+export default async function MarketingPage() {
+	const client = hc<AppType>(env.NEXT_PUBLIC_API_BASE_URL)
+	const res = await client.api.hello.$get({ query: { name: "Hono!" } })
+	const data = await res.json()
+	console.log(data.message)
+
 	return (
 		<>
 			<svg
